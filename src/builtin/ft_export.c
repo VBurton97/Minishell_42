0d080e64c08dec5d4917f77ac242e9c3e1d82304
Hsin Yu Liu <dewseileaf@gmail.com>
2023-02-15 19:58:33 +0100

    comment

---
+++ src/builtin/ft_export.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_export.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sasha <sasha@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/15 19:15:53 by sasha             #+#    #+#             */
+/*   Updated: 2023/02/15 19:57:59 by sasha            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "builtin.h"
+
+/*
+	can take multiple args
+	each has form name=value
+	equal sign cannot be a name
+*/
+int	ft_export(char **argv, t_shell *shell)
+{
+	
+}
+
