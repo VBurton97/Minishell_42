45fd5291a61db6d82ef144677009c406daf0f8bc
Hsin-yu Liu <hsliu@z4r12p5.42lyon.fr>
2023-02-16 11:57:13 +0100

    new func

--- src/builtin/ft_export.c
+++ src/builtin/ft_export.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_export.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sasha <sasha@student.42.fr>                +#+  +:+       +#+        */
+/*   By: hsliu <hsliu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 19:15:53 by sasha             #+#    #+#             */
-/*   Updated: 2023/02/15 19:57:59 by sasha            ###   ########.fr       */
+/*   Updated: 2023/02/16 11:55:57 by hsliu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,72 @@
 */
 int	ft_export(char **argv, t_shell *shell)
 {
+	int		ret;
+	int		i;
+	t_token	*var;
+
+	if (argv[1] == NULL)
+	{
+		write(2, "export: no argument\n", 20);
+		return (0);
+	}
+	ret = 0;
+	i = 1;
+	while (argv[i])
+	{
+		if (ft_illegal_name(argv[i]))
+			ret = 1;
+		else if (ft_strchr(argv[i], '='))
+		{
+			var = ft_new_var(argv[i]);
+			if (var)
+				ft_add_token(&(shell->env_lst), var);
+		}
+		i++
+	}
+	return (ret);
+}
+
+/*
+	str has the form NAME=VALUE, value can be an empty string
+	return NULL, if the str is not valid, or the malloc fails
+*/
+t_token	*ft_new_var(char *str)
+{
+	t_token	*token;
+
+	token = ft_new_token(str, ft_strlen(str));
+	if (token == NULL)
+	{
+		return (NULL);
+	}
+	return (token);
+}
+
+/*
+	name can only cantains alphabet, number and underscore
+	name cannot begin with number
+	if name doesn't end with '=', ignore it
+*/
+int	ft_illegal_name(char *name)
+{
+	int	i;
 	
+	if (ft_is_digit(name[0]))
+	{
+		write(2, "export: not a valid identifier\n", 31);
+		return (0);
+	}
+	i = 0;
+	while (name[i] != '=')
+	{
+		if (!ft_isalnum(name[i]) && name[i] != '_')
+		{
+			write(2, "export: not a valid identifier\n", 31);
+			return (0);
+		}
+		i++;
+	}
+	return (1);
 }
 
